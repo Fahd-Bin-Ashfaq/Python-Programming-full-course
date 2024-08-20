856d437f5bb5bc9d6f6bb00a2505c30677e7e806
Fahad Bin Ashfaq <108400515+Fahd-Bin-Ashfaq@users.noreply.github.com>
2024-08-20 10:12:04 +0500

    Add files via upload

---
+++ week_5/sixth.py
@@ -0,0 +1,23 @@
+for i in range(1,11):
+    print("Hello World")
+
+for i in "apple":
+    print(i)
+
+
+mylist=[10,20,30,40,50,60,70,80,90,100]
+for list in mylist:
+    print(list)
+
+
+for i in range(11):
+    num=input("Enter Number:")
+    print(num)
+
+i =0
+while(i<=5):
+    print(i)
+    i+=1
+
+
+
